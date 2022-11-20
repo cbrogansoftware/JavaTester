9ed26704934da814c247732d2af457c6c33f2c2f
cbrogansoftware <cbrogansoftware@gmail.com>
2022-11-20 08:00:18 -0600

    Various examples of accessing and reorganizing the complex data structure.

--- src/main/java/com/csbTester/complexdatastructures/MainTester.java
+++ src/main/java/com/csbTester/complexdatastructures/MainTester.java
@@ -3,6 +3,8 @@ package com.csbTester.complexdatastructures;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class MainTester {
 
@@ -29,9 +31,40 @@ public class MainTester {
         classLoad.put(student2, classesSpringL);
         classLoad.put(student3, classesSummerL);
 
+        System.out.println("~~~~~~~~~~~~~~~~~~~~");
+
+        System.out.println("Key value pairs accessing specific object properties:");
         classLoad.forEach((key, value) -> {
             //System.out.println(key.getfName() + "=" + value + " "); // Show all 'classes' for student fName
-            System.out.println(key.getfName() + "=" + value.get(2) + " ");
+            System.out.println(key.getfName() + " is taking " + value.get(2) + " ");
+        });
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~");
+
+        System.out.println("Print keys only:");
+        classLoad.forEach((key, value) -> {
+            System.out.println(key.getfName());
+        });
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~");
+
+        System.out.println("No looping. Show specific key hashCode and specific subject: ");
+        System.out.println(classLoad.get(student1).hashCode());
+        System.out.println(classLoad.get(student1).get(1));
+
+
+        //Arrays.stream(ints).filter(x -> x % 2 == 0).forEach(System.out::println);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~");
+        // New map with only first name and specific subject
+        Map<String, String> result = classLoad.entrySet()
+                .stream()
+                .filter(map -> map.getKey().getfName().contains("i".toLowerCase()))
+                .collect(Collectors.toMap(map -> map.getKey().getfName(), map -> map.getValue().get(1)));
+
+        System.out.println("A new map with desired values that were filtered and simplified to contain first name and specific subjects:");
+        result.forEach((key, value) -> {
+            System.out.println(key + " - " + value);
         });
 
     }
